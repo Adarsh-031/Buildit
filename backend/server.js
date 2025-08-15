5a0d70abc42db6075059dac5c4f034a64cf970b4
Adarsha E <adarsh.edurkala33@gmail.com>
2025-08-15 20:33:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,7 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 const app = express();
-app.use(cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"]
-}));
-
-app.options("*", cors());
+app.use(cors());
 
 app.use(express.json());
 
